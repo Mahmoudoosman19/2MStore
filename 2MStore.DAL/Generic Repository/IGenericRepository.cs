9dd896cd82146300d73a7d3a9541f0a89c4ab79a
Lenovo <Mahmoud4501@gmail.com>
2024-09-15 23:41:06 +0300

    Updated Architecture

---
+++ 2MStore.DAL/Generic Repository/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2MStore.DAL.Generic_Repository
+{
+    internal interface IGenericRepository
+    {
+    }
+}
