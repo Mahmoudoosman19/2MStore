73c9883623fefa3b61aa1dce2a02b4c6fd35f005
Lenovo <Mahmoud4501@gmail.com>
2024-09-15 22:02:40 +0300

    Created Models, Entity package installed

---
+++ 2MStore.Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2MStore.Models
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        [ForeignKey("PDetailID")]
+        public int ProductDetails {  get; set; }
+
+
+    }
+}
